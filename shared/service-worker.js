db7bfaf1e304117b5d048b230904fbdffd4f21d9
serhii sh <elrib7vj@gmail.com>
2024-10-01 12:03:56 +0300

    Create service-worker.js

---
+++ shared/service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installing.');
+  // Perform install steps
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activated.');
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('Fetching:', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
